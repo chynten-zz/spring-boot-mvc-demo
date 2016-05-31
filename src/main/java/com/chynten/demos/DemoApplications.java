8b781236eb84302562b7ca7cbc80e2397cfe18ee
Chintan Patel <chintanpatel.it@gmail.com>
2016-05-31 12:25:44 +0530

    Changed package name

--- src/main/java/com/chynten/demos/DemoApplications.java
+++ src/main/java/com/chynten/demos/DemoApplications.java
@@ -1,4 +1,4 @@
-package com.elephantem.demos;
+package com.chynten.demos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/chynten/demos/controller/MyController.java
+++ src/main/java/com/chynten/demos/controller/MyController.java
@@ -1,4 +1,4 @@
-package com.elephantem.demos.controller;
+package com.chynten.demos.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
